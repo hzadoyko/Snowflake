cba46a57800c15da07400c7fc347e988c99bc5f0
Heidi Sorensen <hzadoyko@gmail.com>
2026-01-27 21:05:25 -0500

    Add Cortex queries for data analysis and translation
    Added queries to utilize Cortex for summarization, sentiment analysis, answer extraction, and translation.

---
+++ Using-Cortex-Funct.sql
@@ -0,0 +1,15 @@
+-- Query to use Cortex to summarize the content of a column in a table
+SELECT SNOWFLAKE.CORTEX.SUMMARIZE(reviews)
+FROM menu;
+
+-- Query to use Cortex to extract sentiment of a column in a table
+SELECT SNOWFLAKE.CORTEX.SENTIMENT(reviews)
+FROM menu;
+
+--Query to use Cortex to extract answers from a column in a table
+SELECT SNOWFLAKE.CORTEX.EXTRACT_ANSWER(menu_item_health_metrics_obj, 'What ingredients are in this item?')
+FROM menu;
+
+-- Query to use Cortex to translate a column
+SELECT SNOWFLAKE.CORTEX.TRANSLATE(item_category, 'en', 'es')
+FROM menu;
